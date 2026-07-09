5c8e52486b7612b84dd7905add6b0ce90712489e
viio00 <194786916+viio00@users.noreply.github.com>
2026-07-09 23:49:23 +0800

    update return data type for pinch

--- include/beetle.h
+++ include/beetle.h
@@ -4,7 +4,7 @@
 void run_log(void);
 void run_crawl(const char *device_path);
 void run_trail(const char *file_path);
-void run_pinch(const char *device_name);
+int run_pinch(const char *device_name);
 void run_puncture(const char *file_path);
 
 #endif
