4e4ae1087ec974e3b3d0d287118874d00b515f50
viio00 <194786916+viio00@users.noreply.github.com>
2026-07-21 03:21:40 +0800

    Not yet tested, waiting for crawl

--- src/puncture.c
+++ src/puncture.c
@@ -22,6 +22,11 @@ int run_puncture(const char *file_path) {
 
     printf("[*] Puncture: Tagret %s is %ld bytes. Initiating shred...\n", file_path, file_size);
 
+    /*
+    srand = Seed Random. By calling srand(time(NULL)), you use the current 
+    calendar time (in seconds) as the Seed
+    - This is like the initializer for rand()
+    */
     srand((unsigned int)time(NULL));
 
     for (long i = 0; i < file_size; i++) {
@@ -30,10 +35,32 @@ int run_puncture(const char *file_path) {
     }
 
     fflush(file);
-    int fd = fileno(file);
-    fsync(fd);
+    /*
+    fflush() grabs everything (bytes from byte-by-byte in your loop) currently sitting in C's private
+    waiting room and forces it out into the Operating System's memory (the kernel cache).
 
-    fclose(file);
+    The status: The data is out of your C program, 
+    but it is still just sitting in the RAM of your OS. It isn't on the SSD yet.
+    */
+    int fd = fileno(file);
+    /*
+    FILE* is a high-level pointer, so the kernel does not understand this. The kernel needs a low-level
+    integer ID which is the File Descriptor (fd)
+
+    fileno() asks the standard library, "Hey, what is the low-level integer ID that the OS 
+    assigned to this file?" -- The ID is created and handed out by the Operating System Kernel
+
+    The status: You now have the raw integer handle (fd) needed to speak directly to the OS system calls.
+    */
+    fsync(fd); //This is the ultimate brute-force command.
+    /*
+    tells the Operating System kernel: "Stop waiting. Drop everything you are doing, freeze, 
+    and physically commit these bytes directly onto the hardware sectors right now."
+
+    The status: The random noise bytes are now permanently stamped onto the physical SSD/HDD sectors.
+    */
+
+    fclose(file); 
 
     if (unlink(file_path) == 0) {
         printf("[*] Puncture success. %s permanently shredded.", file_path);
